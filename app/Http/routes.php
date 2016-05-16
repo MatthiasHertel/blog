cdbe73f06b904c7c91d7c924d142552468aa033f
Matthias Hertel <me@matthiashertel.eu>
2016-05-16 19:08:11 +0200

    static pages ... about contact home

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('contact', 'PagesController@getContact');
+Route::get('about', 'PagesController@getAbout');
+Route::get('/', 'PagesController@getIndex');
