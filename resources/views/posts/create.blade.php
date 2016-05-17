10514feaebe960e2bf0beb14839541a326e375a5
Matthias Hertel <me@matthiashertel.eu>
2016-05-17 19:11:22 +0200

    enhanced with js validation parsley ... simple to use nice feedback to users

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,10 @@
 @extends('main')
 
-@section('title', '| Create New Post');
+@section('title', '| Create New Post')
+
+@section('stylesheets')
+  {!! Html::style('css/parsley.css') !!}
+@endsection
 
 @section('content')
 
@@ -9,12 +13,12 @@
         <h1>Create New Post</h1>
         <hr>
 
-        {!! Form::open(array('route' => 'posts.store')) !!}
+        {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
           {{ Form::label('title', 'Title:') }}
-          {{ Form::text('title', null, array('class' => 'form-control')) }}
+          {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
           {{ Form::label('body', 'Post Body:') }}
-          {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+          {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
           {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top:20px;')) }}
         {!! Form::close() !!}
@@ -22,4 +26,8 @@
       </div>
     </div>
 
+@endsection
+
+@section('scripts')
+  {!! Html::script('js/parsley.min.js') !!}
 @endsection
