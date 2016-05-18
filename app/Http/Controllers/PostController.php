f1569a3475096e50504970150db32cd427b24dd1
Matthias Hertel <me@matthiashertel.eu>
2016-05-18 08:15:30 +0200

    posts.show ... view and pass the $post to the with after create u could see the item

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -68,7 +68,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        return view('posts.show');
+        $post = Post::find($id);
+
+        return view('posts.show')->withPost($post);
     }
 
     /**
