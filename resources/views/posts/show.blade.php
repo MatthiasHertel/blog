9aa7fb5e4f0f351091687c7c43cc8b3699981e0b
Matthias Hertel <me@matthiashertel.eu>
2016-05-18 21:13:59 +0200

    posts.show ... sidebar date (create and update) and action buttons (edit and delete)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,38 @@
 
 @section('content')
 
-    <h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+    <div class="row">
+      <div class="col-md-8">
+        <h1>{{ $post->title }}</h1>
 
+        <p class="lead">{{ $post->body }}</p>
+
+      </div>
+      <div class="col-md-4">
+        <div class="well">
+
+            <dl class="dl-horizontal">
+              <dt>Created At: </dt>
+              <dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>Last Updated: </dt>
+              <dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+            </dl>
+            <hr>
+            <div class="row">
+              <div class="col-sm-6">
+                {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+                <!-- <a href="#" class="btn btn-primary btn-block">Edit</a> -->
+              </div>
+              <div class="col-sm-6">
+                {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+                <!-- <a href="#" class="btn btn-danger btn-block">Delete</a> -->
+              </div>
+            </div>
+
+        </div>
+      </div>
+    </div>
 
 @endsection
