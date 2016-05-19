d3220dd3fe60ff564f04f17e2ffc402a3a3c4ece
Matthias Hertel <me@matthiashertel.eu>
2016-05-19 23:30:33 +0200

    edit view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -83,7 +83,9 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('posts.edit')->withPost($post);
     }
 
     /**
