2ceb391e0b60ace948bcc2ba27a64757cc620fa3
Matthias Hertel <me@matthiashertel.eu>
2016-05-23 16:13:42 +0200

    add middleware auth ... to deny access .. it is set to auth not to guest ... so guest cannot access postcontroller ... and were redirect to login page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,6 +12,10 @@ use Session;
 
 class PostController extends Controller
 {
+
+    public function __construct() {
+      $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
