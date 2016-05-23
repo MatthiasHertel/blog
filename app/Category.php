5e8a4f6efe1ca3868a5e8c466a2cf3626dc1a934
Matthias Hertel <me@matthiashertel.eu>
2016-05-23 21:43:18 +0200

    set the name and the relation to the Post model (foreign key)

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // set the name because category -> categories no convention
+    protected $table = 'categories';
+
+    // set the connection to the Post model
+    public function posts()
+    {
+      return $this->hasMany('App\Post');
+    }
 }
