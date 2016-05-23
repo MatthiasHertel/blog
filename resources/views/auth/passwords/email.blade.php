99b3c1d5bde8e82d12d254f2182316b5ed679532
Matthias Hertel <me@matthiashertel.eu>
2016-05-23 19:35:06 +0200

    the two forms (views) - passwort reset ... and confirm new password

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,31 @@
+@extends('main')
+
+@section('title', '| Forgot my Password')
+
+@section('content')
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+
+      <div class="panel panel-default">
+        <div class="panel-heading"> Reset Password</div>
+        <div class="panel-body">
+          @if (session('status'))
+            <div class="alert alert-success">
+              {{ session('status') }}
+            </div>
+          @endif
+          {!! Form::open(['url' => 'password/email', 'method' => "POST"]) !!}
+
+          {{ Form::label('email', 'Email Adress:') }}
+          {{ Form::email('email', null, ['class' => 'form-control']) }}
+
+          {{ Form::submit('Reset Password', ['class' => 'btn btn-primary btn-h1-spacing']) }}
+
+          {{ Form::close() }}
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+@endsection
---
+++ resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,34 @@
+@extends('main')
+
+@section('title', '| Forgot my Password')
+
+@section('content')
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+
+      <div class="panel panel-default">
+        <div class="panel-heading"> Reset Password</div>
+        <div class="panel-body">
+
+          {!! Form::open(['url' => 'password/reset', 'method' => "POST"]) !!}
+          {{ Form::hidden('token', $token) }}
+
+          {{ Form::label('email', 'Email Adress:') }}
+          {{ Form::email('email', $email, ['class' => 'form-control']) }}
+
+          {{ Form::label('password', 'New Password:') }}
+          {{ Form::password('password', ['class' => 'form-control'])}}
+
+          {{ Form::label('password_confirmation', 'Confirm New Password:') }}
+          {{ Form::password('password_confirmation', ['class' => 'form-control'])}}
+
+          {{ Form::submit('Reset Password', ['class' => 'btn btn-primary btn-h1-spacing']) }}
+
+          {{ Form::close() }}
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+@endsection
