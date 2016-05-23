8cbc8ee618821c5719ec80d58108cac8ec234e68
Matthias Hertel <me@matthiashertel.eu>
2016-05-23 22:10:48 +0200

    set categorie routes ... except create .. because is it in index route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,6 +25,10 @@ Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
 Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
 Route::post('password/reset', 'Auth\PasswordController@reset');
 
+// Categories
+Route::resource('categories', 'CategoryController', ['except' => ['create']]);
+
+
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
 
