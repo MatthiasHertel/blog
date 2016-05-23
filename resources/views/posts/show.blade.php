71920380b24a52f86be14edfac695f197e36ad21
Matthias Hertel <me@matthiashertel.eu>
2016-05-23 23:37:35 +0200

    add label to wellbox category to show post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,6 +19,10 @@
               <p><a href="{{ route('blog.single', $post->slug)}}">{{ route('blog.single', $post->slug)}}</a></p>
             </dl>
             <dl class="dl-horizontal">
+              <label>Category: </label>
+              <p> {{ $post->category->name }}</p>
+            </dl>
+            <dl class="dl-horizontal">
               <label>Created At: </label>
               <p>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</p>
             </dl>
