38f9199fc83de38ffc92bd8e7a4f6469be14a1f2
Matthias Hertel <me@matthiashertel.eu>
2016-05-23 11:17:09 +0200

    fix the show view .. correct url with blog/

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,7 +15,8 @@
         <div class="well">
             <dl class="dl-horizontal">
               <label>Url Slug: </label>
-              <p><a href="{{ url($post->slug) }}">{{ url($post->slug) }}</a></p>
+              <!-- <p><a href="{{ url('blog/'.$post->slug) }}">{{ url('blog/'.$post->slug) }}</a></p> -->
+              <p><a href="{{ route('blog.single', $post->slug)}}">{{ route('blog.single', $post->slug)}}</a></p>
             </dl>
             <dl class="dl-horizontal">
               <label>Created At: </label>
