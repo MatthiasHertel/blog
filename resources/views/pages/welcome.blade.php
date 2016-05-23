91d332a83f63487aa532540afdaee56e0f11c276
Matthias Hertel <me@matthiashertel.eu>
2016-05-23 11:16:37 +0200

    fix the read more button ...

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -19,7 +19,7 @@
               <p>
                 {{ substr($post->body, 0, 300) }} {{ strlen($post->body) > 300 ? "..." : "" }}
               </p>
-              <a href="#" class="btn btn-primary">Read More</a>
+              <a href="{{ url('blog/'.$post->slug) }}" class="btn btn-primary">Read More</a>
             </div>
             <hr>
             @endforeach
