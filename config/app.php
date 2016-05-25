c1204cfe1affc8d8d4a4b5a28a0101317a027020
Matthias Hertel <me@matthiashertel.eu>
2016-05-25 15:26:44 +0200

    install "intervention/image": "^2.3" ... to resize and crop userphoto

--- config/app.php
+++ config/app.php
@@ -157,6 +157,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Codecourse\Notify\NotifyServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class
+
 
     ],
 
@@ -205,6 +207,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class
+
 
     ],
 
